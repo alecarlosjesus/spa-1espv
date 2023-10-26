d3d96910e6f21322356a2215a68b2f5bd5f0cc14
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-26 20:45:00 -0300

    Correção do CP2

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import "./index.module.css";
@@ -9,13 +8,15 @@ import Home from "./routes/Home.jsx";
 import Produtos from "./routes/Produtos.jsx";
 import EditarProdutos from "./routes/EditarProdutos.jsx";
 import Error404 from "./routes/Error404.jsx";
-
+ 
 const router = createBrowserRouter([
   {path: "/", element:<App/>,errorElement:<Error404/>, 
     children:[
       {path:"/" , element:<Home/>},
       {path:"/produtos" , element:<Produtos/>},
       {path:"/editar/produtos/:id" , element:<EditarProdutos/>},
+      {path:"/minharota" , element:<Home/>},
+
     ]}
 ]);
 
